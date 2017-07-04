a74a281c351003c2689f3fdd4bc59983548e9621
Branden Olson <bolson2@stoat.fhcrc.org>
2017-07-03 18:54:54 -0700

    Add comparison of nearest neighbor distance distributions

--- SummaryStats.R
+++ SummaryStats.R
@@ -5,19 +5,38 @@ library(shazam)
 library(stringdist)
 library(textmineR)
 
-get.distance.vector <- function(distance.matrix) {
-    vec <- distance.matrix[distance.matrix %>% lower.tri] %>% as.vector
+get.distance.matrix <- function(sequence.list) {
+    distance.matrix <- sequence.list %>% stringdistmatrix %>% as.matrix
+    return(distance.matrix)
+}
+
+get.distance.vector <- function(sequence.list) {
+    mat <- sequence.list %>% get.distance.matrix
+    vec <- mat[mat %>% lower.tri] %>% as.vector
     return(vec)
 }
 
 compare.pairwise.distance.distribution <- function(list.a, list.b) {
-    distance.vector.a <- stringdistmatrix(list.a) %>% as.matrix %>% get.distance.vector
-    distance.vector.b <- stringdistmatrix(list.b) %>% as.matrix %>% get.distance.vector
+    distance.vector.a <- list.a %>% get.distance.vector
+    distance.vector.b <- list.b %>% get.distance.vector
     divergence <- CalcJSDivergence(distance.vector.a, distance.vector.b)
     return(divergence)
 }
 
+get.nearest.neighbor.distances <- function(sequence.list) {
+    mat <- sequence.list %>% get.distance.matrix
+    n <- sequence.list %>% length
+    distances <- rep(NA, n)
+    for(i in 1:n) {
+        distances[i] <- mat[i, which.min(mat[i, -i])]
+    }
+    return(distances)
+}
+
 compare.NN.distance.distribution <- function(list.a, list.b) {
-     
+    distances.a <- get.nearest.neighbor.distances(list.a) 
+    distances.b <- get.nearest.neighbor.distances(list.b)
+    divergence <- CalcJSDivergence(distances.a, distances.b)
+    return(divergence)
 }
 
