995816b3a4f47ba2086c05e60db1ec1ae30ddfc1
BrandenOlson <branden.olson@colorado.edu>
2017-09-20 11:55:09 -0700

    Add function to resample dataset with replacement

--- R/Compare.R
+++ R/Compare.R
@@ -1,5 +1,17 @@
 source("R/SummaryStats.R")
 
+#' Resample a dataset with replacement
+#'
+#' \code{resample_data} returns a data.table (or data.frame) object with
+#'   resampled rows from the input dataset. This yields a new dataset with the
+#'   same number of rows and similar characteristics.
+#' @param dat data.table or data.frame object to be resampled
+#' @return A new, resampled data.table or data.frame object, depending on 
+#'   the type of \code{dat}
+resample_data <- function(dat) {
+    return( dat[sample(nrow(dat), replace=TRUE), ] )
+}
+
 #' Run through a full repertoire comparison
 #'
 #' \code{compareRepertoires} iterates through the various comparison functions
