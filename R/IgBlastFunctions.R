02069c20b1b0cfb3ae77a06774c6d20c9d37599d
BrandenOlson <branden.olson@colorado.edu>
2018-01-04 07:50:13 -0800

    Parse igblast output with changeo and return annotation dataset

--- R/IgBlastFunctions.R
+++ R/IgBlastFunctions.R
@@ -1,8 +1,12 @@
-#' Run IgBLAST on an input fasta file
+library(data.table)
+
+#' Get IgBLAST annotations from an input fasta file
 #'
 #' Currently kind of hacky, as it changes directories to the location of
-#'   igblast and its germline databases, runs the command, and changes back
-#'   to the initial directory
+#'   igblast and its germline databases, runs some commands, and changes back
+#'   to the initial directory.
+#'   Change-O is used to parse the raw output of igblastn.
+#'   TODO: Delete interim files, try-catch the success of changeo
 #'
 #' @param input_filename The path to the input fasta file
 #' @param output_filename The desired output filename WITHOUT an extension
@@ -20,15 +24,17 @@ getIgBlastAnnotations <- function(input_filename,
 
     input_filename <- input_filename %>% 
         normalizePath
+    igblast_dir <- "~/Software/igblast/bin"
+    changeo_dir <- "~/.local/bin"
 
     # IgBlast needs to be in its own directory to find the germline databases
     # easily, so let's remember where we are currently and chnage directories
     initial_wd <- getwd()
-    igblast_dir <- "~/Software/igblast/bin"
     igblast_dir %>% setwd
     igblast_exec <- file.path(igblast_dir, "igblastn")
     arguments <- igblast_exec
-    for(region in c("V", "D", "J")) {
+    regions <- c("V", "D", "J")
+    for(region in regions) {
         arguments <- c(arguments, 
                      paste0("-germline_db_", region),
                      file.path(
@@ -39,6 +45,11 @@ getIgBlastAnnotations <- function(input_filename,
                                )
                      )
     }
+
+    full_output_filename <- 
+                   paste(output_filename,
+                         "fmt7",
+                         sep='.')
 
     arguments <- c(arguments,
                    "-auxiliary_data",
@@ -61,9 +72,7 @@ getIgBlastAnnotations <- function(input_filename,
                    # input file, since we have changed directories
                    input_filename %>% normalizePath, 
                    "-out",
-                   paste(output_filename,
-                         "fmt7",
-                         sep='.')
+                   full_output_filename
                    )
     
     command <- arguments %>%
@@ -71,7 +80,35 @@ getIgBlastAnnotations <- function(input_filename,
 
     command %>% system
 
+    changeo_command <- paste(file.path(changeo_dir,
+                                       "MakeDb.py"),
+                             "igblast",
+                             "-i",
+                             full_output_filename,
+                             "-s",
+                             input_filename,
+                             "-r",
+                             regions %>%
+                                 sapply(tolower) %>%
+                                 sapply(function(x) {
+                                         paste0(locus,
+                                                '/',
+                                                locus,
+                                                x,
+                                                '.fasta')
+                                        }
+                                 ) %>%
+                                 paste(collapse=' ')
+                             )
+
+    changeo_command %>% system
+    changeo_filename <- output_filename %>%
+        paste0("_db-pass.tab")
+    annotations <- changeo_filename %>%
+        data.table::fread(stringsAsFactors=TRUE)
+
+    # Go back to initial working directory
     initial_wd %>% setwd
 
-    return(command)
+    return(annotations)
 }
