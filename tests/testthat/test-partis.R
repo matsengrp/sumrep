7725bf98457b3ae12593481238b10cb9b2f9e56a
BrandenOlson <branden.olson@colorado.edu>
2018-07-02 10:39:10 -0700

    Quick partis test fix

--- tests/testthat/test-partis.R
+++ tests/testthat/test-partis.R
@@ -4,17 +4,20 @@ context("Partis annotations and partitions")
 test_data_path <- system.file("data/test_data.fa", package="sumrep")
 
 test_that("sumrep correctly calls partis annotate", {
-    dat_a <- annotateSequences(test_data_path, do_full_annotation=FALSE)
-    dat_b <- annotateSequences(test_data_path, output_filename="blah.csv",
-        output_path="_output_arbitrary", num_procs = 8)
+    dat_a <- annotateSequences(test_data_path, 
+                               do_full_annotation=FALSE)
+    dat_b <- annotateSequences(test_data_path, 
+                               output_filename="blah.csv",
+                               output_path="_output_arbitrary", 
+                               num_procs=8)
 
     expect_equal(dat_a %>% names, c("annotations", "mutation_rates"))
     expect_equal(dat_a$mutation_rates[[1]] %>% names, 
                  c("overall_mut_rate", "mut_rate_by_position"))
 
-    expect_equal(ncol(dat_a$annotations), 29)
+    expect_equal(ncol(dat_a$annotations), 36)
     expect_equal(nrow(dat_a$annotations), 17)
-    expect_equal(ncol(dat_b$annotations), 43)
+    expect_equal(ncol(dat_b$annotations), 45)
     expect_equal(nrow(dat_b$annotations), 17)
 
     dir.create("_output")
@@ -24,8 +27,10 @@ test_that("sumrep correctly calls partis annotate", {
 })
 
 test_that("sumrep correctly calls partis partition", {
-    dat <- partitionSequences(test_data_path, output_filename="blah2.csv",
-                              output_path="_output_arbitrary_again", num_procs=8)
+    dat <- partitionSequences(test_data_path, 
+                              output_filename="blah2.csv",
+                              output_path="_output_arbitrary_again", 
+                              num_procs=8)
 
     expect_equal(dat %>% names, c("logprob", "n_clusters", "n_procs", 
                                   "partition"))
