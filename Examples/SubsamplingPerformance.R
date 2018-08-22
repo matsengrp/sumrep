0339bd3b8e3e1aec088cbcd84a5188d21bc1782a
BrandenOlson <branden.olson@colorado.edu>
2018-08-22 14:54:10 -0700

    Clean and abstract subsampling perf example

--- Examples/SubsamplingPerformance.R
+++ Examples/SubsamplingPerformance.R
@@ -1,69 +1,87 @@
 devtools::load_all()
 
+getTrueDistributionDatasetInfo <- function(dat,
+                                           summary_function,
+                                           ...
+                                           ) {
+    pt <- proc.time()
+    true_dist <- summary_function(dat,
+                                  ...) 
+    true_time <- (proc.time() - pt)["elapsed"]
+    true_dat <- cbind(true_dist, "True") %>%
+        data.frame %>%
+        setNames(c("Value", "Setting"))
+    return(list(true_dat=true_dat, true_time=true_time))
+}
+
 loadNewDatasets("data/Annotations")
 
 do_dists <- TRUE
 if(do_dists) {
-    dat <- p_f1$annotations %>% subsample(5000)
+    dat <- p_f1$annotations %>% subsample(10000)
+    distribution_function <- getPairwiseDistanceDistribution
+
+    # Get true distribution
+    pt <- proc.time()
+    true_info <- getTrueDistributionDatasetInfo(dat,
+                                               distribution_function,
+                                               column="cdr3s",
+                                               approximate=FALSE) 
+    true_dat <- true_info$true_dat
+    true_time <- true_info$true_time
     
+    # Get subsampled distributions for various tolerances
     times <- {}
     distributions <- list()
     tols <- 10^seq(-1, -7)
     
     for(i in 1:length(tols)) { pt <- proc.time()
-        distributions[[i]] <- getPairwiseDistanceDistribution(dat$cdr3s,
+        distributions[[i]] <- getPairwiseDistanceDistribution(dat,
+                                                              column="cdr3s",
                                                               approximate=TRUE,
                                                               tol=tols[i])
         times[i] <- (proc.time() - pt)["elapsed"]
     }
     
-    pt <- proc.time()
-    true_dist <- getPairwiseDistanceDistribution(dat$cdr3s,
-                                                 approximate=FALSE) 
-    true_time <- (proc.time() - pt)["elapsed"]
-    true_dat <- cbind(true_dist, "True", 500) %>%
-        data.frame %>%
-        setNames(c("Value", "Setting", "Smoothness"))
-    
     dist_dat <- distributions %>%
         lapply(data.frame) %>%
-        Map(cbind, ., paste("Tol = ", tols), smooth) %>%
-        lapply(setNames, c("Value", "Setting", "Smoothness")) %>%
+        Map(cbind, ., paste("Tol = ", tols)) %>%
+        lapply(setNames, c("Value", "Setting")) %>%
         do.call(rbind, .) %>%
         rbind(., true_dat)
     
+    print("Hi")
     dist_dat$Value <- as.numeric(dist_dat$Value)
-    dist_dat$Smoothness <- as.numeric(dist_dat$Smoothness)
 }
     
-p1 <- ggplot(dist_dat,
+dist_plot <- ggplot(dist_dat,
        aes(x=as.numeric(Value), group=Setting, colour=Setting)) +
     geom_density(adjust=3) +
     xlim(0, 60) +
     xlab("Pairwise Distance") +
     ylab("Density")
-
-p2 <- ggplot(dist_dat,
-       aes(x=as.numeric(Value), group=Setting, colour=Setting, adjust=Smoothness)) +
-    stat_ecdf() +
-    xlim(0, 60)
+ggsave("~/Manuscripts/sumrep-ms/Figures/dists_by_tol.pdf", width=10)
 
 time_dat <- data.frame(Time=times, Tolerance=tols)
-p3 <- ggplot(d=time_dat, aes(x=log10(Tolerance), y=Time)) +
+time_plot <- ggplot(d=time_dat, aes(x=log10(Tolerance), y=Time)) +
     geom_point() +
     geom_hline(yintercept=true_time, colour="red") +
-    geom_text(aes(0, true_time, label="Time for full dataset", hjust=1, vjust=-1)) +
+    geom_text(aes(0, true_time, label="Time for full dataset", hjust=1, 
+                  vjust=-1)) +
     xlab("Log_10(tolerance)") +
     ylab("Time (seconds)") +
     ggtitle("Time complexity of distribution subsampling by tolerance")
 
-
-p3_log_log <- ggplot(d=time_dat, aes(x=log10(Tolerance), y=log(Time))) +
+log_time_plot <- ggplot(d=time_dat, aes(x=log10(Tolerance), y=log(Time))) +
     geom_point() +
     geom_hline(yintercept=log(true_time), colour="red") +
-    geom_text(aes(0, log(true_time), label="Log(Time) for full dataset", hjust=1, vjust=-1)) +
+    geom_text(aes(0, log(true_time), label="Log(Time) for full dataset", 
+                  hjust=1, vjust=-1)) +
     xlab("Log_10(Tolerance)") +
     ylab("Log(time) (log-seconds)") +
-    ggtitle("Time complexity (in log-seconds) of distribution subsampling by tolerance")
+    ggtitle(
+     "Time complexity (in log-seconds) of distribution subsampling by tolerance"
+    )
 
-multiplot(plotlist=list(p3, p3_log_log), cols=2)
+m <- multiplot(plotlist=list(time_plot, log_time_plot), cols=2)
+ggsave("~/Manuscripts/sumrep-ms/Figures/time_by_tol.pdf", width=10)
