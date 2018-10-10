6f9291df2881a4855a31549e5ff5f0ff6d99fd54
BrandenOlson <branden.olson@colorado.edu>
2018-10-10 14:26:52 -0700

    Update, clean up partis tests

--- tests/testthat/test-partis.R
+++ tests/testthat/test-partis.R
@@ -4,39 +4,43 @@ context("Partis annotations and partitions")
 test_data_path <- system.file("data/test_data.fa", package="sumrep")
 
 test_that("sumrep correctly calls partis annotate", {
-    dat_a <- annotateSequences(test_data_path, 
-                               do_full_annotation=FALSE)
-    dat_b <- annotateSequences(test_data_path, 
-                               output_filename="blah.csv",
-                               output_path="_output_arbitrary", 
-                               num_procs=8)
+    dat_a <- getPartisAnnotations(test_data_path, 
+                                  do_full_annotation=FALSE)
+    "_output" %>% unlink(recursive=TRUE)
+
+    dat_b <- getPartisAnnotations(test_data_path, 
+                                  output_filename="blah.csv",
+                                  output_path="_output_arbitrary", 
+                                  num_procs=8)
+    "_output_arbitrary" %>% unlink(recursive=TRUE)
 
     expect_equal(dat_a %>% names, c("annotations", "mutation_rates"))
     expect_equal(dat_a$mutation_rates[[1]] %>% names, 
                  c("overall_mut_rate", "mut_rate_by_position"))
 
-    expect_equal(ncol(dat_a$annotations), 36)
+    expect_equal(ncol(dat_a$annotations), 38)
     expect_equal(nrow(dat_a$annotations), 17)
-    expect_equal(ncol(dat_b$annotations), 45)
+    expect_equal(ncol(dat_b$annotations), 38)
     expect_equal(nrow(dat_b$annotations), 17)
 
     dir.create("_output")
     file.create("_output/blah.txt")
-    expect_error(annotateSequences(test_data_path))
+    expect_error(getPartisAnnotations(test_data_path))
     "_output" %>% unlink(recursive=TRUE)
 })
 
 test_that("sumrep correctly calls partis partition", {
-    dat <- partitionSequences(test_data_path, 
+    dat <- getPartisPartitions(test_data_path, 
                               output_filename="blah2.csv",
                               output_path="_output_arbitrary_again", 
                               num_procs=8)
+    "_output_arbitrary_again" %>% unlink(recursive=TRUE)
 
     expect_equal(dat %>% names, c("logprob", "n_clusters", "n_procs", 
                                   "partition"))
 
     dir.create("_output")
     file.create("_output/blah2.txt")
-    expect_error(partitionSequences(test_data_path))
+    expect_error(getPartisPartitions(test_data_path))
     "_output" %>% unlink(recursive=TRUE)
 })
