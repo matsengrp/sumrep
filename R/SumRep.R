eb82ad10b0a03202a0dd76f63257992ddf53e653
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2019-01-14 14:47:30 -0800

    Fixed some missing imports.

--- R/SumRep.R
+++ R/SumRep.R
@@ -8,19 +8,24 @@
 #' @import ape
 #' @import Biostrings
 #' @import CollessLike
+#' @import crayon
 #' @import data.table
 #' @import dplyr
 #' @import entropy
+#' @import grid
 #' @import HDMD
 #' @import jsonlite
 #' @import magrittr
+#' @import methods
 #' @import pegas
 #' @import Peptides
 #' @import RecordLinkage
 #' @import seqinr
 #' @import shazam
+#' @import stats
 #' @import stringdist
 #' @import stringr
 #' @import textmineR
+#' @import utils
 #' @import yaml
 NULL
