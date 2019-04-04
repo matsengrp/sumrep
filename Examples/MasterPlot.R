7f627d55d0079d74ff97a526a67ad02b3039c559
BrandenOlson <branden.olson@colorado.edu>
2019-04-04 15:51:44 -0700

    Add more datasets to master plot example

--- Examples/MasterPlot.R
+++ Examples/MasterPlot.R
@@ -1,6 +1,20 @@
 loadNewDatasets("data/Annotations")
 
-pdf("~/Manuscripts/sumrep-ms/Figures/master_plot.pdf", width=14, height=14)
-p_f1$annotations %>%
-    plotDistributions(tall_plot=TRUE)
-dev.off()
+dataset_list <- list(p_f1$annotations,
+                     p_f1_sim$annotations,
+                     p_g1$annotations,
+                     p_g1_sim$annotations
+                    )
+plot_names <- c("p_f1", "p_f1_sim", "p_f2", "p_f2_sim")
+
+p <- plotUnivariateDistributions(dataset_list,
+                            plot_type="freqpoly",
+                            names=plot_names
+                           )
+ggsave("~/Manuscripts/sumrep-ms/Figures/master_plot_freqpoly.pdf", width=14, height=14)
+
+p <- plotUnivariateDistributions(dataset_list,
+                                 plot_type="ecdf",
+                                 names=plot_names
+                                )
+ggsave("~/Manuscripts/sumrep-ms/Figures/master_plot_ecdf.pdf", width=14, height=14)
